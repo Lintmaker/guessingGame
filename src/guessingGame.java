669b869fb9eb57295e79203731d24b114f595080
Lin Yao Pan <lpan7932@bths.edu>
2018-10-12 14:49:46 -0400

    Objective 2

--- src/guessingGame.java
+++ src/guessingGame.java
@@ -4,6 +4,7 @@ public class guessingGame
     public static void main (String [] args)
     {
         Scanner input = new Scanner(System.in);
+        String response = "";
         double generated = Math.random();
         int x = (int)(generated * 11);
         System.out.print("Welcome to the guessing game.");
@@ -23,7 +24,25 @@ public class guessingGame
             System.out.print("If you change your mind, you have to run the whole thing again and play level one :)");
         }
         System.out.print("Welcome to the jungle. Please select a difficulty.");
-        // Input "type 1", "type 2" "type 3" or "type 4"
-        
+        // Input "type 2" or "type 3"
+        String type = input.nextLine();
+        if (type == "type 3")
+        {
+            System.out.print("Pick a number between 1 and 100. ");
+            System.out.print( "We'll guess what it is and keep score based on how many guesses it takes us");
+            int number = input.nextInt();
+            guess = 50;
+            int points = 1;
+            while (response != "correct");
+            {
+                response = input.nextLine();
+                if (response == "higher")
+                {
+                    guess = 75;
+                    points += 1;
+                }
+            }
+
+        }
     }
 }
