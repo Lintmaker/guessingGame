c1917cd7a215e086f576a53dea815f5af01b398d
Lin Yao Pan <lpan7932@bths.edu>
2018-10-12 14:34:48 -0400

    Had to restart

--- src/guessingGame.java
+++ src/guessingGame.java
@@ -1,21 +1,29 @@
 import java.util.Scanner;
 public class guessingGame
 {
-    public static void guessingGame(String num)
+    public static void main (String [] args)
     {
-        int number = 0;
-        String diff = "";
         Scanner input = new Scanner(System.in);
-        System.out.print("type 1,2,3, or 4"); // please type: type num
-        diff = input.nextLine(); // please type: type
-        if (diff.equals("type 1"))
+        double generated = Math.random();
+        int x = (int)(generated * 11);
+        System.out.print("Welcome to the guessing game.");
+        System.out.print(" Guess what the answer is from 0 - 10");
+        int guess = input.nextInt();
+        while (guess != x)
         {
-            System.out.print("pick a number from 0 - 100");
-            number = input.nextInt();
-            while (number != (int guess))
-            {
-
-            }
+        System.out.print("Guess again");
+        guess = input.nextInt();
         }
+        System.out.print("You did it");
+
+        System.out.print("Do you want to try something different");
+        String playAgain = input.nextLine();
+        if (playAgain.equals("no"))
+        {
+            System.out.print("If you change your mind, you have to run the whole thing again and play level one :)");
+        }
+        System.out.print("Welcome to the jungle. Please select a difficulty.");
+        // Input "type 1", "type 2" "type 3" or "type 4"
+        
     }
 }
