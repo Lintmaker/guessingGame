0303656ff9c7d5cfe8f2b6a0d00bc264cf9c3cd2
Lin Yao Pan <lpan7932@bths.edu>
2018-10-13 21:18:01 -0400

    Finally Done!

--- src/objective2.java
+++ src/objective2.java
@@ -1,46 +1,65 @@
 import java.util.Scanner;
-public class objective2
-{
-    public static void main (String [] args)
-    {
-       System.out.print("input type.");
-        Scanner input = new Scanner (System.in);
+public class objective2<points> {
+    public static void main(String[] args) {
+        System.out.print("input type.");
+        Scanner output = new Scanner(System.in);
         String response = "";
-        int guess = 0;
-
+        response = output.nextLine();
+        if (response.equals("type3"))
+        {
             System.out.print("Pick a number between 1 and 100. ");
-            System.out.print( "We'll guess what it is and keep score based on how many guesses it takes us");
-            int number = input.nextInt();
+            System.out.print("We'll guess what it is and keep score based on how many guesses it takes us");
+            int number = output.nextInt();
             int min = 1;
             int max = 100;
-            guess = ((int)(min + max)/2);
+            int guess = ((min + max) / 2);
             System.out.print(guess);
-            System.out.print("higher, lower, or correct");
-            response = input.nextLine();
+            System.out.print(" higher, lower, or correct");
             int points = 1;
-            while (!response.equals("correct"))
-            {
-                if (response.equals("higher"))
-                {
+            while (number != guess) {
+                response = output.nextLine();
+                if (response.equals("higher")) {
                     min = guess;
-                    guess = (int)(min + max)/2;
-                   System.out.print(guess);
-                    response = input.nextLine();
-                    points +=1;
-                }
-                else if (response.equals("lower"))
-                {
-                    max = guess;
-                    guess = (int) (min + max)/2;
-                    System.out.println(guess);
-                    response = input.nextLine();
+                    guess = ((min + max) / 2);
+                    System.out.print(guess);
                     points += 1;
+                } else if (response.equals("lower")) {
+                    max = guess;
+                    guess = ((min + max) / 2);
+                    points += 1;
+                    System.out.print(guess);
                 }
-                System.out.print("You're score is ");
-                System.out.print(points);
-
             }
-            System.out.print("why");
+            System.out.print(" You got a score of ");
+            System.out.print(points);
         }
-    }
-
+        else
+            {
+                System.out.print("Pick a number between 1 and 10. ");
+                System.out.print("We'll guess what it is and keep score based on how many guesses it takes us");
+                int number = output.nextInt();
+                int min = 1;
+                int max = 10;
+                int guess = ((min + max) / 2);
+                System.out.print(guess);
+                System.out.print(" higher, lower, or correct");
+                int points = 1;
+                while (number != guess) {
+                    response = output.nextLine();
+                    if (response.equals("higher")) {
+                        min = guess;
+                        guess = ((min + max) / 2);
+                        System.out.print(guess);
+                        points += 1;
+                    } else if (response.equals("lower")) {
+                        max = guess;
+                        guess = ((min + max) / 2);
+                        points += 1;
+                        System.out.print(guess);
+                    }
+                }
+                System.out.print(" You got a score of ");
+                System.out.print(points);
+            }
+            }
+}
